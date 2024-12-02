f1773c4a87d34820e7736d55a085a5363b901dbb
oxxultus <a38027928@gmail.com>
2024-12-02 23:43:06 +0900

    실행 파일 추가

---
+++ src/main/java/schoolSystemManagement/App.java
@@ -0,0 +1,8 @@
+package schoolSystemManagement;
+
+public class App {
+    public static void main(String[] args) {
+        Management management = new Management();
+        management.setVisible(true);
+    }
+}
